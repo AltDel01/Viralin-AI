4b73c3c67853b0372edf7ec4dca3883d53ca56a5
AltDel01 <putra.ekadarma@gmail.com>
2026-03-08 20:29:38 +0700

    Initial commit

---
+++ src/data/galleryImages.ts
@@ -0,0 +1,137 @@
+export const feedImages = [
+  {
+    src: "/images/gallery/feed-1.png",
+    alt: "Herbal Tea Daily Boost Ad"
+  },
+  {
+    src: "/images/gallery/feed-2.png",
+    alt: "Special Banana Chips Promotion"
+  },
+  {
+    src: "/images/gallery/feed-3.png",
+    alt: "CareYou Lip Balm Product"
+  },
+  {
+    src: "/images/gallery/feed-4.png",
+    alt: "Together We Can Ocean Protection"
+  },
+  {
+    src: "/images/gallery/feed-5.png",
+    alt: "Elegant House Real Estate"
+  },
+  {
+    src: "/images/gallery/feed-6.png",
+    alt: "Hot & Spicy Pizza"
+  },
+  {
+    src: "/images/gallery/feed-7.png",
+    alt: "New Collection Limited Edition Sneakers"
+  },
+  {
+    src: "/images/gallery/feed-8.png",
+    alt: "Nostalgia Mini Album"
+  },
+  {
+    src: "/images/gallery/feed-9.png",
+    alt: "Espresso Coffee Ad"
+  },
+  {
+    src: "/images/gallery/feed-10.png",
+    alt: "Aleca Beauty Summer Sale"
+  },
+  {
+    src: "/images/gallery/feed-11.png",
+    alt: "Healthy Breakfast With Cereal"
+  },
+  {
+    src: "/images/gallery/feed-12.png",
+    alt: "Stay Hydrated With Gulp Stor"
+  },
+  {
+    src: "/images/gallery/feed-13.png",
+    alt: "Lumivera Radiant Serum"
+  },
+  {
+    src: "/images/gallery/feed-14.png",
+    alt: "Explore Thailand"
+  },
+  {
+    src: "/images/gallery/feed-15.png",
+    alt: "Earth Hour Awareness"
+  },
+  {
+    src: "/images/gallery/feed-16.png",
+    alt: "Borcelle Gourmet Restaurant"
+  },
+  {
+    src: "/images/gallery/feed-17.png",
+    alt: "Marine by Keithston & Co"
+  },
+  {
+    src: "/images/gallery/feed-18.png",
+    alt: "National Day Celebration of Cereal"
+  },
+  {
+    src: "/images/gallery/feed-19.png",
+    alt: "Sol Janeiro Brazilian Summer Products"
+  }
+];
+
+export const storyImages = [
+  {
+    src: "/images/gallery/story-1.png",
+    alt: "Chocolate Ice Cream Cone - Story format"
+  },
+  {
+    src: "/images/gallery/story-2.png",
+    alt: "Coming Soon Fashion - Story format"
+  },
+  {
+    src: "/images/gallery/story-3.png",
+    alt: "Golden Crunch Cookie - Story format"
+  },
+  {
+    src: "/images/gallery/story-4.png",
+    alt: "Sports Research Collagen - Story format"
+  },
+  {
+    src: "/images/gallery/story-5.png",
+    alt: "Premium Cashews - Story format"
+  },
+  {
+    src: "/images/gallery/story-6.png",
+    alt: "Turmeric Powder - Story format"
+  },
+  {
+    src: "/images/gallery/story-7.png",
+    alt: "Auto Detailing Services - Story format"
+  },
+  {
+    src: "/images/gallery/story-8.png",
+    alt: "Pills Medication - Story format"
+  },
+  {
+    src: "/images/gallery/story-9.png",
+    alt: "Cheese Lover's Day Promo - Story format"
+  },
+  {
+    src: "/images/gallery/story-10.png",
+    alt: "Cocktail Party - Story format"
+  },
+  {
+    src: "/images/gallery/story-11.png",
+    alt: "New Arrival Fashion - Story format"
+  },
+  {
+    src: "/images/gallery/story-12.png",
+    alt: "We Are Brewing - Story format"
+  },
+  {
+    src: "/images/gallery/story-13.png",
+    alt: "Delicious Ice Cream - Story format"
+  },
+  {
+    src: "/images/gallery/story-14.png",
+    alt: "Pastries and Croissants - Story format"
+  }
+];
